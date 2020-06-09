fa34f21deddbd6676e057dafb3be9b97f4f46231
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2020-06-09 15:59:19 -0500

    Update plot_PROCAR.sh

--- Programas_Auxiliares/plot_PROCAR.sh
+++ Programas_Auxiliares/plot_PROCAR.sh
@@ -14,7 +14,7 @@ EfermiDown=0
 
 Title=Ir_{2}               #Gnuplot generará un .png llamado 'Title.png'
                               #La imagen generada tendrá el mismo título
-AutomaticRange=false              #Utiliza el rango automático de gnuplot
+AutomaticRange=true              #Utiliza el rango automático de gnuplot
                         #Si 'true' entonces se  inhabilita lo siguiente:
 xmin=-6          #Cota mínima del rango en X de la gráfica que generará
 xmax=6           #Cota máxima del rango en X de la gráfica que generará
@@ -32,40 +32,38 @@ NombreScript=Script_gplot  #Se genera un script llamado 'NombreScript'
 #########################################################################
 
 
-###### Se obtienen las energias ##########
-grep  "energy" PROCAR  | awk '{print $5}' > energias  #PROCAR-->$1
-###### Se obtiene el número de átomos #####
-Nat=$(head -1 poscar.xyz )
 ###### Se obtienen los elementos ##########
 to_xyz POSCAR > poscar.xyz
+Nat=$(head -1 poscar.xyz )
 tail -$Nat poscar.xyz | awk '{print $1}' > elementos
-
 ##### Obtiene la energía de Fermi #########
 if [ -f DOSCAR ]
 then
    EfermiUp=$(head -6 DOSCAR | tail -1 | awk '{print $4}')
    EfermiDown=$(head -6 DOSCAR | tail -1 | awk '{print $4}')
 fi
+
+##### Divide el archivo en 2: unopara spinup y otro para down
+nl=$(echo $(wc -l PROCAR | awk '{print $1}')/2 | bc)
+tail -$nl PROCAR > procar.down
+head -$nl PROCAR > procar.up
+##### Obtiene las energias para ambos espines
+grep  "energy" procar.up  | awk '{print $5}' > energias_up  #PROCAR-->$1
+grep  "energy" procar.down  | awk '{print $5}' > energias_down  #PROCAR-->$1
+
 ##### Se generan todos los archivos individuales listos para graficar ###
 
 for ((i=1;i<$(($Nat+1));i++))
 do
 
    # Desglosa los archivos por átomo por orbital por espín
-   grep -A $Nat "ion " PROCAR  | grep " $i " | awk '{print $2}' > #atomo_$i_orbital_s aux
-   grep -A $Nat "ion " PROCAR  | grep " $i " | awk '{print $3}' > #atomo_$i_orbital_p aux
-   grep -A $Nat "ion " PROCAR  | grep " $i " | awk '{print $4}' > #atomo_$i_orbital_d aux
+   grep -A $Nat "ion " procar.up  | grep " $i " | awk '{print $2}' > atomo${i}_orbitals_up.aux
+   grep -A $Nat "ion " procar.up  | grep " $i " | awk '{print $3}' > atomo${i}_orbitalp_up.aux
+   grep -A $Nat "ion " procar.up  | grep " $i " | awk '{print $4}' > atomo${i}_orbitald_up.aux
 
-############ AGREGAR LO DEL SPIN DOWN :grep -A $(($Nat*2)) la primeramitad es up
-########### la segunda mitad es down debajo de cada ion.
- > atomo${i}_orbitals_up.aux
- > atomo${i}_orbitals_down.aux
-
- > atomo${i}_orbitalp_up.aux
- > atomo${i}_orbitalp_down.aux
-
- > atomo${i}_orbitald_up.aux
- > atomo${i}_orbitald_down.aux
+   grep -A $Nat "ion " procar.down  | grep " $i " | awk '{print $2}' > atomo${i}_orbitals_down.aux
+   grep -A $Nat "ion " procar.down  | grep " $i " | awk '{print $3}' > atomo${i}_orbitalp_down.aux
+   grep -A $Nat "ion " procar.down  | grep " $i " | awk '{print $4}' > atomo${i}_orbitald_down.aux
 
    # Desplaza el cero a la energía de Fermi
    echo "awk '{print \$1-$EfermiUp}' atomo${i}_orbitals_up.aux " | bash > atomo${i}_orbital_s_up
@@ -78,27 +76,24 @@ do
    echo "awk '{print \$1-$EfermiDown}' atomo${i}_orbitald_down.aux " | bash > atomo${i}_orbital_d_down
 
    # Junta la información con el tipo de atomo y las energías
-   paste energias atomo${i}_orbital_s_up > atomo${i}_orbital_s_up.dat
-   paste energias atomo${i}_orbital_s_down > atomo${i}_orbital_s_down.dat
+   paste energias_up atomo${i}_orbital_s_up > atomo${i}_orbital_s_up.dat
+   paste energias_down atomo${i}_orbital_s_down > atomo${i}_orbital_s_down.dat
 
-   paste energias atomo${i}_orbital_p_up > atomo${i}_orbital_p_up.dat
-   paste energias  atomo${i}_orbital_p_down > atomo${i}_orbital_p_down.dat
+   paste energias_up atomo${i}_orbital_p_up > atomo${i}_orbital_p_up.dat
+   paste energias_down  atomo${i}_orbital_p_down > atomo${i}_orbital_p_down.dat
 
-   paste energias atomo${i}_orbital_d_up > atomo${i}_orbital_d_up.dat
-   paste energias atomo${i}_orbital_d_down > atomo${i}_orbital_d_down.dat
+   paste energias_up atomo${i}_orbital_d_up > atomo${i}_orbital_d_up.dat
+   paste energias_down atomo${i}_orbital_d_down > atomo${i}_orbital_d_down.dat
 
    # Elimina archivos residuales (auxiliares)
-   rm atomo${i}_orbital_s_up.aux atomo${i}_orbital_s_up
-   rm atomo${i}_orbital_s_down.aux atomo${i}_orbital_s_down
-
-   rm atomo${i}_orbital_p_up.aux atomo${i}_orbital_p_up
-   rm atomo${i}_orbital_p_down.aux atomo${i}_orbital_p_down
-
-   rm atomo${i}_orbital_d_up.aux atomo${i}_orbital_d_up
-   rm atomo${i}_orbital_d_down.aux atomo${i}_orbital_d_down
+   rm atomo${i}_orbitals_up.aux atomo${i}_orbital_s_up
+   rm atomo${i}_orbitals_down.aux atomo${i}_orbital_s_down
+   rm atomo${i}_orbitalp_up.aux atomo${i}_orbital_p_up
+   rm atomo${i}_orbitalp_down.aux atomo${i}_orbital_p_down
+   rm atomo${i}_orbitald_up.aux atomo${i}_orbital_d_up
+   rm atomo${i}_orbitald_down.aux atomo${i}_orbital_d_down
 
 done
-
 
 #***********************************************************************#
 #                      ESCRIBE SCRIPTS PARA GNUPLOT                     #
@@ -123,11 +118,8 @@ set xrange [$xmin:$xmax]
 ">> $NombreScript
 fi
 
-
-
 echo "set style fill transparent solid 0.3 noborder ">> $NombreScript
 echo -n "plot  " >> $NombreScript
-
 
 for ((l=1;l<$(($Nat+1));l++))
 do
@@ -135,32 +127,31 @@ do
 #   multiplicidad=$( ls $prefix.pdos.pdos_atm#$l\(* | wc -l ) #Cuenta el numero de orbitales tiene el l-esimo atomo
    for j in _s _p _d
    do
-      fileup=  .dat
-      filedown=  .dat
-
+      fileup=$(ls atomo${l}_orbital*${j}_up.dat)
+      filedown=$(ls atomo${l}_orbital*${j}_down.dat)
 #*********************Parser que determina el tipo de Átomo**********************#
 
-      tipo=$( head -$i elementos | tail -1 )
+      tipo=$( head -$l elementos | tail -1 )
 
 #*********************************************************************#
 #                     Asigna colores                                  #
 #********************************************************************#
       color=$(grep "$tipo" colores | grep "$j" | awk '{print $2}')
-
+echo $l $j $tipo $color
 #**************************** Spin up ************************************#
 
-        echo -n "\"$fileup\" u 1:2 w filledcurve below lt rgb " >> $NombreScript
-        echo -n " \"$color\" notitle, " >> $NombreScript
+        echo -n "\"${fileup}\" u 1:2 w filledcurve below lt rgb " >> $NombreScript
+        echo -n " \"${color}\" notitle, " >> $NombreScript
 
 #*************************** Spin down ***********************************#
 
-        echo -n "\"$filedown\" u 1:2  w filledcurve below lt rgb " >> $NombreScript
+        echo -n "\"${filedown}\" u 1:2  w filledcurve below lt rgb " >> $NombreScript
         echo -n " \"$color\" notitle, " >> $NombreScript
 
    done
 done 2>/dev/null
 gnuplot $NombreScript
-
+rm energias elementos procar.down procar.up poscar.xyz
 
 echo "Se utilizaron los siguientes colores"
 cat colores

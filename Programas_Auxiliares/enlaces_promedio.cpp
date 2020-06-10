d608f793f4f06b8aeac49e512e63d4e531188d4b
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2020-06-09 21:04:29 -0500

    Update enlaces_promedio.cpp

--- Programas_Auxiliares/enlaces_promedio.cpp
+++ Programas_Auxiliares/enlaces_promedio.cpp
@@ -1,9 +1,27 @@
 #include<atomicpp.h>
 
-Cluster clus;
+double distancias;
+int contador;
+double suma;
+
 int main()
 {
-
-
+Cluster clus;
+clus.read_xyz("cluster.xyz");
+suma=0;
+contador=0;
+for(i=0;i<clus.Nat;i++)
+{
+   for(j=0;j<clus.Nat;j++)
+   {
+      distancias=Atomic_Distance(clus.atom[i],clus.atom[j]);
+      if(distancias<2.95 && distancias >1.0)
+      {
+      contador++;
+      suma=distancias+suma;
+      }
+   }
+}
+cout<<"Enlace promedio: "<<suma/contador<<endl;
 return 0;
 }
